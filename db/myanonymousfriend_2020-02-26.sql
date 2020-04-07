308b595f67fb1bc9b9f107ef7733d9b99f90c199
Solenir <solenir.figueredo@gmail.com>
2020-04-07 13:57:38 -0300

    alteração script

--- db/myanonymousfriend_2020-02-26.sql
+++ db/myanonymousfriend_2020-02-26.sql
@@ -1,200 +1,46 @@
-# ************************************************************
-# Sequel Pro SQL dump
-# Version 4541
-#
-# http://www.sequelpro.com/
-# https://github.com/sequelpro/sequelpro
-#
-# Host: 127.0.0.1 (MySQL 5.7.16)
-# Database: DB_myanonymousfriend
-# Generation Time: 2020-02-27 01:43:47 +0000
-# ************************************************************
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 -- *****************************************************
 -- CRIACAO DO BANCO DE DADOS
 -- *****************************************************
+
 CREATE DATABASE DB_myanonymousfriend
 DEFAULT CHARACTER SET utf8
 DEFAULT COLLATE utf8_general_ci;
 
--- *****************************************************
--- CRIACAO DAS TABELAS
--- *****************************************************
 
 USE DB_myanonymousfriend;
 
-# Dump of table chat
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `chat`;
-
-CREATE TABLE `chat` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `remetente_user_id` int(11) NOT NULL,
-  `destinatario_user_id` int(11) NOT NULL,
-  `status_id` int(11) NOT NULL,
-  `tipo_chat_id` int(11) NOT NULL,
-  `message` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+-- *****************************************************
+-- Tabela..........: USUARIO
+-- Elemento do DER.: Entidade Usuario
+-- *****************************************************
+CREATE TABLE IF NOT EXISTS USUARIO(
+  ID_Usuario INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  Nome VARCHAR(50) NOT NULL,
+  Login VARCHAR(40) NOT NULL UNIQUE KEY,
+  Senha VARCHAR(62) NOT NULL,
+  logado BOOLEAN NOT NULL DEFAULT FALSE
+)ENGINE = INNODB;
 
 
+-- *****************************************************
+-- Tabela..........: PUBLICAÇÃO
+-- Elemento do DER.: Entidade Publicação
+-- *****************************************************
+CREATE TABLE IF NOT EXISTS PUBLICACAO (
+  ID_Publicacao INT NOT NULL AUTO_INCREMENT PRIMARY KEY,  
+  Descricao TEXT
+)ENGINE = INNODB;
 
-# Dump of table migrations
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `migrations`;
-
-CREATE TABLE `migrations` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `batch` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-LOCK TABLES `migrations` WRITE;
-/*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
-
-INSERT INTO `migrations` (`id`, `migration`, `batch`)
-VALUES
-	(1,'2014_10_12_000000_create_users_table',1),
-	(2,'2014_10_12_100000_create_password_resets_table',1),
-	(3,'2020_02_21_235746_create_pessoa_table',2),
-	(4,'2020_02_21_235818_create_chat_table',2),
-	(5,'2020_02_21_235939_create_tipo_chat_table',2),
-	(6,'2020_02_21_235954_create_tipo_pessoa_table',2),
-	(7,'2020_02_22_000042_create_publicacao_table',2),
-	(8,'2020_02_22_113127_create_status_table',2);
-
-/*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
-UNLOCK TABLES;
-
-
-# Dump of table password_resets
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `password_resets`;
-
-CREATE TABLE `password_resets` (
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  KEY `password_resets_email_index` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-
-
-# Dump of table pessoa
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `pessoa`;
-
-CREATE TABLE `pessoa` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `tipo_pessoa_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `nome` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-
-
-# Dump of table publicacao
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `publicacao`;
-
-CREATE TABLE `publicacao` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` int(11) NOT NULL,
-  `message` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-
-
-# Dump of table status
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `status`;
-
-CREATE TABLE `status` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `descricao` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-
-
-# Dump of table tipo_chat
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `tipo_chat`;
-
-CREATE TABLE `tipo_chat` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `descricao` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-
-
-# Dump of table tipo_pessoa
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `tipo_pessoa`;
-
-CREATE TABLE `tipo_pessoa` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `descricao` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-
-
-# Dump of table users
-# ------------------------------------------------------------
-
-DROP TABLE IF EXISTS `users`;
-
-CREATE TABLE `users` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `users_email_unique` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-
-
-
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- *****************************************************
+-- Tabela..........: MENSAGEM
+-- Elemento do DER.: Entidade Mensagem
+-- *****************************************************
+CREATE TABLE IF NOT EXISTS MENSAGEM (
+  ID_Mensagem INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  idRemetente INT NOT NULL,
+  idDestinatario INT NOT NULL,
+  Texto TEXT,
+  HoraMensagem DATETIME,
+  FOREIGN KEY (idRemetente) REFERENCES USUARIO(ID_Usuario),
+  FOREIGN KEY (idDestinatario) REFERENCES USUARIO(ID_Usuario)
+)ENGINE = InnoDB;
